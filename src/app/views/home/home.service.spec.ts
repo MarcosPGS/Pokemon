b64911fb23ce4c2d073e7525eaa2e2d43cee9291
Marcos Paulo <marcospsilva222@hotmail.com>
2021-01-12 13:04:22 -0300

    teste home service

--- src/app/views/home/home.service.spec.ts
+++ src/app/views/home/home.service.spec.ts
@@ -2,15 +2,39 @@ import { TestBed } from '@angular/core/testing';
 
 import { HomeService } from './home.service';
 
+import { BrowserAnimationsModule, NoopAnimationsModule } from '@angular/platform-browser/animations';
+
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+
 describe('HomeService', () => {
   let service: HomeService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports:[HttpClientTestingModule, MatSnackBarModule, BrowserAnimationsModule, NoopAnimationsModule],
+      providers: [HomeService]
+    });
     service = TestBed.inject(HomeService);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('Listar Cards', () => {
+    expect(service.listarTodosCards()).toBeTruthy();
+  });
+
+  it('Buscar por nome', () => {
+    const nome = 'Reshiram & Charizard-GX';
+    expect(service.buscarPorNome(nome)).toBeTruthy();
+  });
+
+
+  it('Buscar por id', () => {
+    const id = 'sm10-217';
+    expect(service.buscarPorId(id)).toBeTruthy();
   });
 });
